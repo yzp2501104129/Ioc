547487d23ba8c55752fb1162053568189084db66
2501104129@qq.com <2501104129@qq.com>
2023-03-21 21:41:24 +0800

    111
    Signed-off-by: 2501104129@qq.com <2501104129@qq.com>

---
+++ IOC-手写代码/IOCInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioc
+{
+    public interface IOCInterface
+    {
+        void RegisterType<FORM, TO>() where FORM : TO;
+
+        From Resolve<From>();
+    }
+}
---
+++ IOC-手写代码/IOCclass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioc
+{
+    public class IOCclass : IOCInterface
+    {
+        private static Dictionary<string, object> keyValues = new Dictionary<string, object>();
+
+        public void RegisterType<From, TO>() where From : TO
+        {
+            keyValues.Add(typeof(From).FullName, typeof(From));
+        }
+
+        public From Resolve<From>()
+        {
+            Object OBJ = keyValues[typeof(From).FullName];
+            return (From)Resolve2(OBJ);
+
+        }
+        private object Resolve2(object OBJ)
+        {
+            var ConstructorsList = OBJ.GetType().GetConstructors();
+            var Ctor = ConstructorsList.FirstOrDefault(x => x.IsDefined(typeof(IOCAttribute), true));
+            if (Ctor == null)
+            {
+                Ctor = ConstructorsList.OrderByDescending(x => x.GetParameters().Length).First();
+            }
+            List<object> listobj = new List<object>();
+            foreach (var item in Ctor.GetParameters())
+            {
+                var param = keyValues[item.ParameterType.FullName];
+                Resolve2(param);
+                listobj.Add(param);
+            }
+            return Activator.CreateInstance(OBJ.GetType(), listobj.ToArray());
+        }
+    }
+}
